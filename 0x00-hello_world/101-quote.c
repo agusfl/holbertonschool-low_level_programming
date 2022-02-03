7bedc0305d843579dce69a1916b37645775743f8
agusfl <agusflom@hotmail.com>
2022-02-03 10:44:23 -0800

    9 task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *main - print a message with puts
+ *
+ *Return: end program
+ */
+int main(void)
+{
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+			return (1);
+}
