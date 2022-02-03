278012a56d9a5da02ed95c29cfa6070083d656a5
agusfl <agusflom@hotmail.com>
2022-02-03 08:57:56 -0800

    4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#!/bin/bash
-int main () {
-	char str[] = "/"Programming is like building a multilingual puzzle/n";
-	puts(str);
+#include <stdio.h>
 
-	return 0;
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main (void)
+		{
+	puts("\"Programming is like building a multilingual puzzle/n");
+
+	return (0);
+		}
