2072f88b12fb5d03a24f88bc1a6568a734ab00c7
agusfl <agusflom@hotmail.com>
2022-02-03 09:24:34 -0800

    borrar

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  *main - print a message with puts
+ *   *
+ *    *Return: end program
+ *     */
+int main(void)
+{
+	                printf("with proper grammar, but the outcome is a piece of art,\n");
+			                        return (0);
+}
