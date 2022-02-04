badcadf50c38e039a1052fc685dbc1654ddd8930
agusfl <agusflom@hotmail.com>
2022-02-04 11:28:24 -0800

    10 task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (d = 0; d <= 9; d++)
 	{
-	for (j = (d + 1); j <= 9; j++)
-	{
-	putchar(d + '0');
-	putchar(j + '0');
-	if (d != 8 || j != 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
+		for (j = (d + 1); j <= 9; j++)
+		{
+			putchar(d + '0');
+			putchar(j + '0');
+				if (d != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
 	}
 	putchar('\n');
 	return (0);
