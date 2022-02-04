a6161289e826e48212de1bd34edc18be97ed62eb
agusfl <agusflom@hotmail.com>
2022-02-04 11:21:18 -0800

    10 task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - print a message with printf
+ *
+ *Return: end program
+ */
+int main(void)
+{
+	int d, j;
+
+	for (d = 0; d <= 9; d++)
+	{
+	for (j = (d + 1); j <= 9; j++)
+	{
+	if (d != j)
+	{
+	putchar(d + '0');
+	putchar(j + '0');
+	if (d != 8 || j != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
