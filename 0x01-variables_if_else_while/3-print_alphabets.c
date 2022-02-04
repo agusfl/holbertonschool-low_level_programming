8e4292f0bb71371a14b9ade720a2024e944b49f9
agusfl <agusflom@hotmail.com>
2022-02-04 07:21:19 -0800

    3 task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print a message with printf
+ *
+ *Return: end program
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
