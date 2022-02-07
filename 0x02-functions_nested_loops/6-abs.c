0f42b2bb88970e5c3a31ded292a29c0fb53447be
agusfl <agusflom@hotmail.com>
2022-02-07 08:43:49 -0800

    6 task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * abs - checks for lowercase character
+ * @n: character to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	_putchar('\n');
+}
