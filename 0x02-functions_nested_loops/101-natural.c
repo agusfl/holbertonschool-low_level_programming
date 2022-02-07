8d34b7d3f1f76afd68b96bcb44fb1bc79e16f3d8
agusfl <agusflom@hotmail.com>
2022-02-07 14:20:39 -0800

    13 task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - sum of multiples for 3 or 5
+ * Return: Zero
+ */
+
+int main(void)
+{
+	int i;
+	int suma = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			suma = i + i;
+		}
+	}
+	printf("%d\n", suma);
+	return (0);
+}
