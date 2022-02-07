375a09e92e6b892ce6934b450a54380c43627ef7
agusfl <agusflom@hotmail.com>
2022-02-07 04:47:35 -0800

    1 task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,24 @@
  *main -code
  *
  *Return: always 0
- */
+*/
+void print_alphabet(void);
 int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ *print_alphabet -code
+ *
+ *Return: always 0
+*/
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	putchar('\n');
-	return (0);
+	putchar(ch);
 }
