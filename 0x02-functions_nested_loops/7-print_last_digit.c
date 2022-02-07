8ae7354f4820c25a146cb30a251d40b38fbd2cf8
agusfl <agusflom@hotmail.com>
2022-02-07 09:11:52 -0800

    7 task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for lowercase character
+ * @n: character to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (n < 0)
+	{
+	_putchar(ld*-1)
+		return (0);
+	}
+	else 
+	{
+	_putchar(ld);
+	}
+	_putchar('\n');
+}
