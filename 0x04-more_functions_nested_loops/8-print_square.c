4018e25dc51f9a1827362ea89008521ab77ae692
agusfl <agusflom@hotmail.com>
2022-02-09 08:28:00 -0800

    8 task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - checks for lowercase character
+ * @size: square size
+ * Return: square
+ */
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size >= 1)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
