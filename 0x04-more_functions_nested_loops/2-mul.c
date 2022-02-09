64924257ed66ccad15b386bc4b6dbe0a7d6d1766
agusfl <agusflom@hotmail.com>
2022-02-09 04:54:54 -0800

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - add two integers
+ * @num1: integer argument num1
+ * @num2: integer argument num2
+ * Return: multiplication
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
