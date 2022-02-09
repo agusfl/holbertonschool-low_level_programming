ae66396611f4d90a474892ffb1a224b26ff89f97
agusfl <agusflom@hotmail.com>
2022-02-09 11:17:57 -0800

    10 task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - checks for lowercase character
+ * @size: character to check
+ * Return: triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < (size - i); j++)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
