401eb43e41a0f81d24bf6fef5b66e0f8c1a7ef63
agusfl <agusflom@hotmail.com>
2022-02-09 09:52:54 -0800

    9 task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 101; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
@@ -21,8 +21,6 @@ int main(void)
 
 		else
 			printf("%d", i);
-		if (i == 100)
-			continue;
 
 		printf(" ");
 	}
