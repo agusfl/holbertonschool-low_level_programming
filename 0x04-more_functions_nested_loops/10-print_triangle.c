2be4fcdaee3d929f506eac8be8d76533f7f96c93
agusfl <agusflom@hotmail.com>
2022-02-09 15:29:05 -0800

    10 task - comments

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,15 +12,30 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 1; i <= size; i++) /** inicializo i en 1 para que tome
+					      * solo los size mayor a cero
+					      */
 		{
-			for (j = 0; j < (size - i); j++)
+			for (j = 0; j < (size - i); j++) /**
+							   * size - 1 nos devuelve los espacions vacios
+							   * ya que nos dice dependiendo en la fila en la que
+							   * estamos que el largo de espacios para que quede
+							   * un triangulo hacia la derecha va a ser el size
+							   * que se le establezca al triangulo menos la fila (i)
+							   * en la que se este
+							   */
 				_putchar(' ');
-			for (j = 0; j < i; j++)
+			for (j = 0; j < i; j++) /** Con este for se ponen los # ya que despues de
+						  * haber puesto los
+						  * espacios en blanco, siempre que j sea menor a i, voy a querer
+						  * poner los # para formar el triangulo.
+						  */
 				_putchar('#');
 			_putchar('\n');
 		}
 	}
 	else
-		_putchar('\n');
+		_putchar('\n'); /** si size es 0 o negativo se imprime un
+				  * salto de linea tal y como pedian
+				  */
 }
