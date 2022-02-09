ec371337d7ca7149c1ee8da45ac7e975c5366635
agusfl <agusflom@hotmail.com>
2022-02-09 06:52:10 -0800

    7 task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - checks for lowercase character
+ * @n: character to check
+ * Return: n lines _
+ */
+
+void print_diagonal(int n)
+{
+	int largo;
+	int blank;
+
+	if (n >= 1)
+	{
+		for (largo = 0; largo < n; largo++)
+		{
+		for (blank = 0; blank < largo; blank++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
