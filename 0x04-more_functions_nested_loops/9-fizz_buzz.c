477c3b775b114074a33ef198d9f8b22b0becce19
agusfl <agusflom@hotmail.com>
2022-02-09 09:41:09 -0800

    9 task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 1 to 100.
+ * Return: numbers in range 1-100 with Fizz instead of 3 and Buzz instead of 5
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
