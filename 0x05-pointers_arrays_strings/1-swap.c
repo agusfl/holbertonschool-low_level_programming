10c7adb8ff8c92623836a74f1f2d24c3c0bad082
agusfl <agusflom@hotmail.com>
2022-02-11 07:56:59 -0800

    1 task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - change value of a variable with a pointer
+ * @a: variable 1
+ * @b: variable2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int ab, ba;
+	ab = *a;
+	ba = *b;
+	*a = ba;
+	*b = ab;
+}
