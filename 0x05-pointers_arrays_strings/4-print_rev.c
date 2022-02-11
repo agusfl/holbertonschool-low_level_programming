4838ae4602ebc11cc4984e278ed41e105b39a4db
agusfl <agusflom@hotmail.com>
2022-02-11 09:23:22 -0800

    4 task with comments

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,23 @@ void print_rev(char *s)
 	int i = 0;
 	int rev = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0') /**
+			       *se crea un puntero de tipo char para la variable s
+			       *es un array que va aumentando hasta llegar a '\0'
+			       *que es el ultimo caracter que se agrega automaticamente
+			       *cuandos e hace un string.
+			       */
 	{
 		i++;
 	}
-	for (rev = i - 1; rev >= 0; rev--)
+	for (rev = i - 1; rev >= 0; rev--) /**
+					     *aca le resto 1 a i para empezar por el final del string
+					     *por eso la ultima condicion va en decremento --
+					     *en el putchar le ponemos s[rev] para que imprima
+					     *caracter por caracter y la condicion es
+					     *mayor igual a 0 ya que arrancamos por la ultima
+					     *posicion del string y no por la primera
+					     */
 	{
 		_putchar(s[rev]);
 	}
