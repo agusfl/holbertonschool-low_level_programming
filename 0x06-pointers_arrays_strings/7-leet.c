195dbacca1dae1e1e99905a2595d428c00beb6f6
agusfl <agusflom@hotmail.com>
2022-02-16 07:58:24 -0800

    7 task with more comments

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,16 +17,25 @@ char *leet(char *str)
 		for (j = 0; j < 10; j++)
 		{
 			if (str[i] == code[j])
+			{
 				str[i] = decode[j];
+				break;
+			}
 		}
 	}
 	/**
 	 * En ese codigo se crean dos arrays, uno con las letras (code) a cambiar
 	 * y otro con las letras del codigo (decode).
 	 * el primer for itera hasta que srt[i] sea distinto del caracter null
-	 * y el segundo for itera por los 10 elementos de los array definidos
-	 * siempre y cuando se igualen los elementos de ambos arrays, se
-	 * cambia a str[i] por el decode correspondiente.
+	 * y el segundo for itera por los 10 elementos del array definido
+	 * en code [] siempre y cuando se igualen los elementos de ambos
+	 * arrays, se cambia a str[i] por el decode correspondiente.
+	 * Esto anda tanto con un break como sin proque no se repite
+	 * el code y el decode dentro del code, el "break" lo que hace
+	 * es que cuando se cumple la condicion no siga iterando de forma
+	 * innecesaria ya que ya encontro el match entocnes no tiene
+	 * porque seguir iterando, con el break sale y vuelve a entrar
+	 * tambien se podria usar un "continue" en este caso.
 	 */
 	return (str);
 }
