dd94c87216db123c89f37d3fdd12d3e0e269d270
agusfl <agusflom@hotmail.com>
2022-02-17 13:06:20 -0800

    2 task with more comments and explanations

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -21,12 +21,19 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = '\0';
 	}
 	/**
-	 * el segundo for que arranca con ; te toma el valor de i con el que termina
-	 * el primer for, por lo tanto si el largo de i termino en 5 en el primer
-	 * for, el segundo for arranca en 4, esto es para completar con el
-	 * null character por sl "n" que pasaron es mayor al string para imprimir
-	 * asi que te imprimiria hasta el largo del string y el resto lo completa
-	 * con el null character
-	 **/
+	 * "src" viene de source, ya que es el input que se quiere copiar
+	 * dentro de "dest" que seria destination osea hacia donde va a copiar
+	 * lo que tenga "src". El primer for se fija que se copien todos los
+	 * caracteres que tenga "src" dentro y el segundo for se completan
+	 * los caracters nulos dependiendo del n que te pasen.
+	 * El segundo for arranca con ; para tomar el valor de i con el que
+	 * termina el primer for, por lo tanto si i termino en 5 en el primer
+	 * for, el segundo for arranca en 6 (ya que se hace el incremento y no
+	 * vuelve a entrar al for), el segundo for es para completar con el
+	 * null character por si el sl "n" que pasaron es mayor al string para
+	 * imprimir asi que te imprimiria hasta el largo del string y el resto
+	 * lo completa con el null character, en el ejemplo del 2-mainc. usan
+	 * un *.
+	 */
 	return (dest);
 }
