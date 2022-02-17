865c8e7978181b9882163c6f8cc8cf14f7924749
agusfl <agusflom@hotmail.com>
2022-02-17 13:19:31 -0800

    0 task with more comments and explanations

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -37,6 +37,15 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[i] = '\0';
 	/**
+	 * En esta funcion lo que se busca es guardar suficiente espacio en
+	 * "dest" (destino) para poder concatenar los dos textos que pasen
+	 * como parametros. Por lo tanto si pasan "Hello" en el primer
+	 * parametro, dest[i] va a estar en 5, al momento de arrancar el for
+	 * si se cumple la condicion de que src[d] sea distinto de null va a
+	 * sobrescribir lo que esta en la posicion 5 de dest (que es el caract
+	 * er null, ya que se empieza a contar del 0 en posiciones) y le va
+	 * poner lo que este en src[0] y asi sucesivamente hasta que src[d]
+	 * sea el caracter null ('\0').
 	 * Con el dest[i] = '\0' le estoy diciendo que cuando este en la ultima
 	 * posicion de dest (que sumo las dos palabras, dest y le agrega src)
 	 * le ponga el null character
