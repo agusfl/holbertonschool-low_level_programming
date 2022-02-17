a803c6a00149bdf5b98d80277d51720f8fb37963
agusfl <agusflom@hotmail.com>
2022-02-17 12:51:51 -0800

    3 task with more comments and explanations

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,12 +19,16 @@ int _strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 
 	/**
-	 * Aca se le dice en el while que si el puntero de s1 y el puntero de s2
-	 * son distintos del caracter null (con el que terminan todos los strings)
-	 * y *s1 es igual a *s2 que vaya sumando tanto s1 como s2.
-	 * En el return se le dice que reste los dos punteros ya que si
-	 * s1 y s2 son iguales, la resta te va a dar cero, si s1 es mayor a s2
-	 * te va a dar positivo y si s1 es menor a s2 te va a dar negativo
-	 * tal y como dice el man de strcmp.
+	 * En el while que si el puntero de s1 y el puntero de
+	 * s2 son distintos del caracter null (con el que terminan todos
+	 * los strings) y *s1 es igual a *s2 que vaya sumando tanto s1 como s2.
+	 * En caso que *s1 no sea igual a *s2 (como es el caso con los
+	 * ejemplos que ponen en 3-main.c para probar el codigo) no entra al
+	 * while ya que no cumple con las condiciones para entrar y va directo
+	 * al return y hace la resta entre *s1 y *s2.
+	 * El puntero s1 tiene el valor de la primer letra que pasan
+	 * en el string 1, que en el ejemplo del 3-main.c es "Hello"
+	 * la "H" en ASCII es 72 y el segundo string palabra es "World!"
+	 * la "W" en ASCII es 87 por lo tanto la resta da: -15.
 	 **/
 }
