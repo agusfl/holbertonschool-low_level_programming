a6b2b6ee8a242afcfbb56671c27f04d4403122ee
agusfl <agusflom@hotmail.com>
2022-02-21 07:21:42 -0800

    0 task without comments

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: memory area
  * @b: constant byte
  * @n: bytes to fill
- * Return: *s
+ * Return: pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -19,9 +19,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-/**
- * Si el n que pasan (que son los bytes para llenar "s" con lo diga "b") es
- * mayor a cero le indico que haga un for donde se fija que i sea menor a n
- * y de ser asi completa s[i] con lo que este en "b".
- * En caso de que n no sea mayor a cero que me devuelva lo que este en "s".
- */
