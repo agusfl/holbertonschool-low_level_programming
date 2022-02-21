d3762caf14ba007c79a70aaf4aea0448b7689345
agusfl <agusflom@hotmail.com>
2022-02-21 07:12:36 -0800

    0 task - different solution and comments

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,18 +12,16 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (n > 0)
+	if (n > 0)
 	{
-		s[i] = b;
-		i++;
-		n--;
+		for (i = 0; i < n; i++)
+			s[i] = b;
 	}
 	return (s);
 }
 /**
- * Hago un while para que se recorra mientras que el n que pasan para llenar
- * con bytes sea mayor a cero y despues dentro del while le indico que vaya
- * copiando lo que esta en la constante "b" a "s" que es el memory area.
- * por eso pongo para que i se incremente y n vaya decrementando hasta que
- * se termina de completar s.
+ * Si el n que pasan (que son los bytes para llenar "s" con lo diga "b") es
+ * mayor a cero le indico que haga un for donde se fija que i sea menor a n
+ * y de ser asi completa s[i] con lo que este en "b".
+ * En caso de que n no sea mayor a cero que me devuelva lo que este en "s".
  */
