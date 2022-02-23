a5136bb7e608ad7a216c408b61305e671b6b31ba
agusfl <agusflom@hotmail.com>
2022-02-23 04:44:43 -0800

    0 task with corrections

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 		_putchar('\n');
-	else if (*s != '\0')
+	return;
+	else
 	{
-		s++;
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(*s + 1);
 	}
-	return (void);
 }
