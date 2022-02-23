53206eceac861116b16f49f154fabd00df95aaf4
agusfl <agusflom@hotmail.com>
2022-02-23 05:39:23 -0800

    1 task WITH comments

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,5 +13,13 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-
+	/**
+	 * Aca lo que se hace es ir apilando en el stack las letras del string
+	 * con cada incremento por el s + 1 y se apila todo hasta que s es
+	 * distinto de null, una vez que esta todo apilado hasta que se llego
+	 * al final del string se empieza a imprimir por el ultimo valor
+	 * apilado ya que el stack se apila con el criterio "LIFO" (Last
+	 * In First Out). Aca tenemos el putchar despues del incremento
+	 * por eso es que se apila todo hasta que llega al caracter null.
+	 */
 }
