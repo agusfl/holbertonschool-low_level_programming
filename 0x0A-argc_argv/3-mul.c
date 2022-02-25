0ede47f0820598b23bb0881f312a3935b3a450c7
agusfl <agusflom@hotmail.com>
2022-02-25 07:13:00 -0800

    3 task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -23,13 +23,4 @@ int main(int argc, char *argv[])
 
 	printf("%d\n", mult);
 	return (0);
-/**
- * El atoi es una funcion de la libreria: stdlib.h y sirve para "parsear" un
- * char a un int, es para indicar que una variable tipo char se trate como un
- * int. En este programa lo que se hace es multiplicar solo dos numeros y en
- * caso de que se pase solo un parametro que se imprima: "Error".
- * En caso de ques se pase mas de 2 argumentos (ademas del nombre del programa
- * que seria el argumento 0) va a multilpicar solo los primero dos numeros que
- * se pase.
- */
 }
