c9861ae1886c0fd2dafdf3a318a2daf11c88ec09
agusfl <agusflom@hotmail.com>
2022-03-03 11:07:18 -0800

    3 task WITHOUT comments

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,20 +13,13 @@ int *array_range(int min, int max)
 	int *arr;
 	int i = 0, len;
 
-	if (min > max)/*dato de letra*/
+	if (min > max)
 	{
 		return (NULL);
 	}
 	len = max - min + 1;
-	/**
-	 * El largo del array va a ser: el maximo menos el minimo + 1
-	 */
 	arr = malloc(sizeof(int) * len);
-	/**
-	 * Se reserva en memoria con malloc espacio para tipo int por el largo
-	 * del array que se pase.
-	 */
-	if (arr == NULL)/*if malloc fails retunr null*/
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
@@ -34,17 +27,5 @@ int *array_range(int min, int max)
 	{
 		arr[i] = min++;
 	}
-	/**
-	 * En el for le indico que recorra desde el i seteado en 0
-	 * hasta el len (largo del array), con la condicion de i menor a len,
-	 * que vaya incrementando de a uno y dentro del for le indico que en
-	 * la posicion a[i] (arranca en cero) le vaya asignando min y que lo
-	 * vaya incrementando de a uno hasta llegar a que se cumpla la
-	 * condicion del largo del array..
-	 */
 	return (arr);
-	/**
-	 * No uso el metodo 'free' porque lo pusieron en el main y si lo pones
-	 * mas de una vez puede ocacionar errores.
-	 */
 }
