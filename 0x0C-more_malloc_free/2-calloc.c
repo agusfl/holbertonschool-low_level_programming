1aabfb1e61b0271848247a14d3ceae92c6b28c2f
agusfl <agusflom@hotmail.com>
2022-03-03 08:34:20 -0800

    2 task with some comments - betty error solved

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -38,17 +38,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		*((char *)arr + i) = 0;
 	}
-	/**
-	 * Hago un for solo para una matriz de dos dimensiones, ya que al poner
-	 * nmemb * size va a recorrer todas las filas y columnas que sean.
-	 * Hacemos un for para setear la memoria de cada elemento del array en
-	 * cero tal y como nos pide la letra, el resultado que printea cuando
-	 * compilamos y ejecutamos con el 2-main.c se imprime en hexadecimal
-	 * porque lo pusieron para que se imprima con ese formato con el printf
-	 * Hay que 'castear' el tipo char porque habiamos creado un puntero
-	 * void, ya que no sabes que tipo de array te van a pasar, se pasa a
-	 * char porque en el 2-main se puede ver que el array que usan es un
-	 * puntero al tipo de dato: char llamado a..
-	 */
 	return (arr);
 }
