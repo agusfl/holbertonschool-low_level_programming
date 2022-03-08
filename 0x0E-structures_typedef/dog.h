474031b3e0db693ff1d754ab28a484e039a3ff4d
agusfl <agusflom@hotmail.com>
2022-03-08 06:55:32 -0800

    dog header, with all the prototypes and structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a structure for dogs
+ * @name: first member of struct
+ * @age: second member of struct
+ * @owner: third member of struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for the struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
