591bae1f3057bfd565589765a97d3e48e3160e9f
agusfl <agusflom@hotmail.com>
2022-03-08 08:24:29 -0800

    2 task with more comments

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,6 @@
 
 void print_dog(struct dog *d)
 {
-
 	if (d != NULL)
 	{
 		if (d->name == NULL)
@@ -30,7 +29,6 @@ void print_dog(struct dog *d)
 		: printf("Owner: %s\n", d->owner);
 	}
 	return;
-
 	/**
 	 * Primero le indico que si la estructura d es distinta de NULL que
 	 * ejecute el codigo ya que no nos pasaron un valor NULL. Pero si nos
@@ -44,5 +42,8 @@ void print_dog(struct dog *d)
 	 * void, y como la edad no puede ser menor a cero es que le pongo que
 	 * imprima '(nil)' en ese caso y si no osea si es igual a cero o mayor
 	 * que imprima la edad.
+	 * Aca hay que imprimir la estructura de Name, Age, Owner con lo que se
+	 * pase en el struct que pongan en el main, en este caso se llama
+	 * 'my_dog'.
 	 */
 }
