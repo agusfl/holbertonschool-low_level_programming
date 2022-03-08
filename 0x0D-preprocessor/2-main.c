6182dea085e73e17cdee68329c42c1b6cbc5e54b
agusfl <agusflom@hotmail.com>
2022-03-08 05:10:48 -0800

    2 task with comments

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file it was compiled from.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+	/**
+	 * __FILE__ es una macro standard predefinida y lo que hace es expandir
+	 * el nombre del "current input file" que se le pase al programa de C.
+	 */
+}
