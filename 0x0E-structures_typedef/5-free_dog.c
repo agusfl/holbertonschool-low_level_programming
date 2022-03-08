fe736d456e3075c253694f8cb8c14afb81e9e72e
agusfl <agusflom@hotmail.com>
2022-03-08 09:11:07 -0800

    5 task try a different solution with return;

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -14,4 +14,11 @@ void free_dog(dog_t *d)
 	free(d->owner);
 	free(d);
 	}
+	return;
+	/**
+	 * Aca no es necesario poner 'return' porque es una funcion de tipo
+	 * void por lo tanto no espera ningun return, se le puede poner return;
+	 * y va a dar un resultado correcto tmb porque es como que no retorna
+	 * nada.
+	 */
 }
