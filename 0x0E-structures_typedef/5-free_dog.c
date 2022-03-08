5b4d85e97954afe980fd1ab1637858a72351c573
agusfl <agusflom@hotmail.com>
2022-03-08 09:02:19 -0800

    5 task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,15 +9,9 @@ void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-		if (d->name)
-		{
-			free(d->name);
-		}
-		if (d->owner)
-		{
-			free(d->owner);
-		}
-		free(d);
+	free(d->name);
+	free(d->owner);
+	free(d);
 	}
 	return (void);
 }
