73a51a9d55fcb9f3a9e8f960ab1969d5fe564ea9
agusfl <agusflom@hotmail.com>
2022-03-08 09:13:33 -0800

    1 task with no return aproach

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,7 +16,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 	}
-	return;
 
 	/**
 	 * Primero se valida que si d es null que salga con return ya que no
