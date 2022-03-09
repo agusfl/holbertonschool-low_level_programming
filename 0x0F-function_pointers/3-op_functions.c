1c52685483e24b5bc409cd644586418e6fc695af
agusfl <agusflom@hotmail.com>
2022-03-09 08:24:35 -0800

    3 task - 3-op_functions.c - archivo donde estan las funciones definidas para suma, resta, div, mul y modulo

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add
+ * @a: num 1
+ * @b: num 2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: num 1
+ * @b: num 2
+ * Return: substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: num 1
+ * @b: num 2
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: num 1
+ * @b: num 2
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus for remainder
+ * @a: num 1
+ * @b: num 2
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
