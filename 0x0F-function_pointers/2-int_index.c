641fab96b0093b64f984eba56bd146cbe3a1c631
agusfl <agusflom@hotmail.com>
2022-03-09 06:51:47 -0800

    1 task with a litlle variant in the first if

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && cmp != NULL && size > 0)
+	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
