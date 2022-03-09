ac09ba6547c3c63c7438ee3d153b07e39a68498b
agusfl <agusflom@hotmail.com>
2022-03-09 07:32:36 -0800

    0 task with more comments

--- 0x0F-function_pointers/0-print_name_WITH_COMMENTS.c
+++ 0x0F-function_pointers/0-print_name_WITH_COMMENTS.c
@@ -7,6 +7,15 @@
  */
 
 void print_name(char *name, void (*f)(char *))
+/**
+ * Definimos una funcion llamada 'print_name' de tipo void que recibe como argumentos:
+ * un puntero a char llamado 'name' y un puntero a una fucnion llamado f de tipo void y que
+ * recibe como argumento un puntero a char.
+ * El primer argumento toma el string (en este caso un nombre) que nos pasen como parametro
+ * al utilizar la funcion que estamos creando 'print_name' y el segundo argumento toma la funcion
+ * que pasen para imprimir el nombre, en estos ejs en el main ves que una imprime el nombre tal y como viene
+ * y la otra la imprime todo en mayuscula (print_name_uppercase).
+ */
 {
 	if (name != NULL && f != NULL)
 		f(name);
@@ -17,6 +26,7 @@ void print_name(char *name, void (*f)(char *))
  * y en caso de que sea null alguno de los dos no se ejecuta nada y se sale de
  * la funcion.
  * En el main de ellos nos pasan las funciones que irian en lugar de 'f', como
- * ser: 'print_name_as_is'.
+ * ser: 'print_name_as_is' o 'print_name_uppercase' ambas son de tipo void al igual
+ * que nuestro puntero a funcion 'f'.
  */
 }
