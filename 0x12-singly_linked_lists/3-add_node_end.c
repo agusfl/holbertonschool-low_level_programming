5635f1e58661f2dd91a5cf2024bee7fe2b277902
agusfl <agusflom@hotmail.com>
2022-03-17 10:49:16 -0700

    Second try

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -47,12 +47,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		*head = new_node;
 	}
-	while (last_node->next != NULL)
+	else
 	{
-		last_node = last_node->next;
-	}
+		last_node = *head;
+		
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
 
-	last_node->next = new_node;
+		last_node->next = new_node;
+	}
 
 	return (new_node);
 }
