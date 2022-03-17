fe5748b668384465f2628d6dafb707b742d559c7
agusfl <agusflom@hotmail.com>
2022-03-17 08:05:49 -0700

    2 task, fiz if new_node == NULL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -32,6 +32,10 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(list_t));
 
 	new_node->str = strdup(str);
