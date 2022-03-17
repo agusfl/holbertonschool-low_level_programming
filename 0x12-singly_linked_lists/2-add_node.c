5520604e73e4d6e7fa0120361833e10c7e062004
agusfl <agusflom@hotmail.com>
2022-03-17 07:55:51 -0700

    2 task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * _strlen - return length of a string
+ * @str: string
+ * Return: length
+ */
+
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: head of the list
+ * @str: string to be added to the list
+ * Return: address of the new element or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
