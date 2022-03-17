77108430acb601bd8826fcf378848447409620c6
agusfl <agusflom@hotmail.com>
2022-03-17 08:33:44 -0700

    100 task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_before_main - Function to execute before main
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
