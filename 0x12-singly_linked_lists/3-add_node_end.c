168191d2b64973c7451a3b5f85dc87c3501aad9f
agusfl <agusflom@hotmail.com>
2022-03-17 10:02:00 -0700

    3 task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * _strlen - return length of a string
+ * @str: string
+ * Return: length
+ */
+
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list
+ * @head: head of the linked list
+ * @str: string to be added to the list
+ * Return: address of the new element or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *last_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+
+	last_node->next = new_node;
+
+	return (new_node);
+}
