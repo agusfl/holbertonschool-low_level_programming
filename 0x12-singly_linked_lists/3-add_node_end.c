877e7f675b91e40d949c3357537b99317f19554c
agusfl <agusflom@hotmail.com>
2022-03-17 10:03:28 -0700

    3 task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -42,7 +42,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		*head = new_node;
