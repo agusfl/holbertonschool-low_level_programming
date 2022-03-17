7eafc6e2f9e26df9fe9ef61ad07854e123f56ef7
agusfl <agusflom@hotmail.com>
2022-03-17 06:40:48 -0700

    0 task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list of our created 
+ * structure: list_t
+ * @h: list of type list_t
+ * Return: numbers of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	unsigned int cont_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		}
+		cont_nodes++;
+	}
+	return (cont_nodes);
+}
