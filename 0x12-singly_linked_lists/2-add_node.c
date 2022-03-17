9933cd023febe7df5fcf3624c5d94dfd768a5671
agusfl <agusflom@hotmail.com>
2022-03-17 08:08:30 -0700

    2 task fix border case

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -32,11 +32,11 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(list_t));
 
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
