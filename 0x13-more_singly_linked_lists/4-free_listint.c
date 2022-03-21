3320e71e5b42b187ce82f3ec41bb63dfbe2d83c4
agusfl <agusflom@hotmail.com>
2022-03-21 05:43:46 -0700

    4 task with some comments and 4 main in case you want to try the code

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint - function to free a linked list of type listint_t
+ * @head: pointer to linked list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	return;
+	/**
+	 * Este ej es distinto al 4 del project 0x12, en este caso estamos
+	 * liberando una lista con integers y hay que hacerlo de esta forma
+	 * para no tener problemas con "make pointer from an integer without a
+	 * cast". Se crea un puntero temporal (tmp) de tipo listint_t, se
+	 * iguala el puntero temporal a head y se itera sobre la lista a la que
+	 * apunta "head", se libera tmp, que tiene asignado lo que habia en
+	 * head antes de iterar por primera vez, luego se hacen las iteraciones
+	 * y se libera hasta que head sea NULL y ya no haya lo que liberar, en
+	 * caso que head sea NULL o este vacio simplemente retornamos para que
+	 * se salga del loop.
+	 */
+}
