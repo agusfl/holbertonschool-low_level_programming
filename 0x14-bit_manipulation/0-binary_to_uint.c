eb2acd77b3d8d005ca56f86011553e9dd3833696
agusfl <agusflom@hotmail.com>
2022-03-24 08:08:53 -0700

    fix of border case

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -32,6 +32,10 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = len - 1; i >= 0; i--)
 	{
+		if (b == NULL)
+		{
+			return (0);
+		}
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
