896b29e5ca811e16b9151cb2d34d485b6005b0c6
agusfl <agusflom@hotmail.com>
2022-03-28 11:06:49 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -43,10 +43,6 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
 
 	write_var = write(fd, text_content, _strlen(text_content));
 	if (write_var == -1 || write_var != _strlen(text_content))
