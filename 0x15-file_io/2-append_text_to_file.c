a90dd8f2975edd04354e87cf03283c88c5449d4d
agusfl <agusflom@hotmail.com>
2022-03-28 11:50:47 -0700

    2 task WITH comments

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,6 +22,13 @@ int _strlen(char *str)
  * @filename: filename for text to be append
  * @text_content: NULL terminated string to append to the file
  * Return: 1 on success and -1 on failure
+ *
+ * Este ej es practicamente igual al ej 1 en lo que respecta a codigo
+ * (cualquier cosa ver comentarios ahi) salvo que hay que poner solo
+ * las flags: O_WRONLY y agregar la flag de append: O_APPEND, se le
+ * saca los permisos para el archivo ya que no estamos creando ningun
+ * archivo nuevo entonces no hay que indicar en este codigo nignun
+ * permiso en especifico ya que no usamos O_CREAT.
  */
 
 int append_text_to_file(const char *filename, char *text_content)
