6c0e4f14b74fc5ce81b31ffaeabd8bca0eab330f
agusfl <agusflom@hotmail.com>
2022-03-28 07:55:39 -0700

    0 task and the text requiscat and 0-main.c in case you want to try the code

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to read
+ * @letters: number of letters
+ * Return: number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_count, write_var;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
+	read_count = read(fd, buff, letters);
+	if (read_count == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	write_var = write(0, buff, read_count);
+	if (write_var == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (read_count);
+}
