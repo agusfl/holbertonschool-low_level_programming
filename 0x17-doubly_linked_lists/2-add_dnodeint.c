6c33fbf922cecefce69322fb466c1c099df56a8c
agusfl <agusflom@hotmail.com>
2022-05-05 08:10:53 -0700

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 
 	new_node->n = n;
 	new_node->next = *head;
