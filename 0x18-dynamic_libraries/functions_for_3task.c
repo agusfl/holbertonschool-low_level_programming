d2e7016cfbe4527276c37ca494881fdb4ec79642
agusfl <agusflom@hotmail.com>
2022-05-09 06:33:57 -0700

    3 task (advanced) all the files for doing this

---
+++ 0x18-dynamic_libraries/functions_for_3task.c
@@ -0,0 +1,61 @@
+#include "main_3task.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: add of 'a' and 'b'
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substraction of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sub of 'a' and 'b'
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiplication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mul of 'a' and 'b'
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - dividition of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: div of 'a' and 'b'
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - modulus of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mod of 'a' and 'b'
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
