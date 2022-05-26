5b63ebddcb5383259b5f11d2c36373feeb84b9c2
agusfl <agusflom@hotmail.com>
2022-05-26 18:55:05 +0000

    good practice

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
+	unsigned long int index = 0;
 
 	index = hash_djb2(key);
 	return (index % size);
