2b3561b8af39f95d1309801d8f18b947ba0b4855
agusfl <agusflom@hotmail.com>
2022-05-27 03:11:30 +0000

    border case

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,6 +11,11 @@ void hash_table_print(const hash_table_t *ht)
 	char *print_comma = "";
 	hash_node_t *node = NULL;
 
+	if (ht == NULL)
+	{
+		return;
+	}
+
 	printf("{");
 	while (i < ht->size)
 	{
