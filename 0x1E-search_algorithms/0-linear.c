c9987b865c1518ce48c4357ac5397117581ec9e5
agusfl <agusflom@hotmail.com>
2022-08-26 01:19:28 +0000

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: Size of the array.
+ * @value: is the value to search
+ * Return: Pointer to the newly created hash table.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	/* Declare variable for iterating */
+	unsigned int i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (i);
+		}
+	i++;
+	}
+	return (-1);
+}
